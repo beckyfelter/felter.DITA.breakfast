dba686d6b8a7cf1b0b519446be5f6765cca8960f
beckyfelter <rfelter@vt.edu>
2017-04-03 11:40:05 -0400

    concepts created

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,10 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "bananabread.html";
+fl["1"]= "biscuitsandgravy.html";
+fl["2"]= "breakfastburrito.html";
+fl["3"]= "omelette.html";
+fl["4"]= "pancakes.html";
+fl["5"]= "vbiscuitsandgravy.html";
+fl["6"]= "veganbreakfastburrito.html";
+var doStem = false;searchLoaded = true;
